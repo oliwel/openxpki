3cd4f001a554bedde1ce6fe69eca702d31ad2c1e
Oliver Welter <github@oliwel.de>
2014-04-08 14:29:08 +0200

    Add config

--- core/server/OpenXPKI/Connector/FakeValidity.pm
+++ core/server/OpenXPKI/Connector/FakeValidity.pm
@@ -10,27 +10,45 @@ use OpenXPKI::Server::Context qw( CTX );
 
 extends 'Connector';
 
+has cut_wday => (
+    is  => 'ro',
+    isa => 'Str',
+    default => 6,
+    );
+    
+has cut_time => (
+    is  => 'ro',
+    isa => 'Str',    
+    default => '17:00',
+    );
+
 sub get {
     my $self = shift;
     my $arg = shift;
     
     my $notafter = OpenXPKI::DateTime::get_validity({
         VALIDITY_FORMAT => 'detect',
-        VALIDITY        => $self->LOCATION(),
+        VALIDITY        => '+0006',# $self->LOCATION(),
     });
     
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime( $notafter->epoch() );          
     
+    my $cut_wday = $self->cut_wday();
+    my @cut_time = split /:/, $self->cut_time();
+    
     # Jump a full week if its saturday but beyond the limit  
-    $wday = -1 if ($wday == 6 && $hour >= 17);    
-    $mday += (6 - $wday);
+    $mday += 7 if ($wday == $cut_wday && $hour >= $cut_time[0] && $min > $cut_time[1]);
+    
+    my $diff_day = ($cut_wday - $wday);
+    if ($diff_day < 0) { $diff_day+=7; }
+    $mday += $diff_day;
     
     my $validity = DateTime->new( 
         year      => $year+1900,
-        month     => $mon,
+        month     => $mon+1,
         day       => $mday,
-        hour      => $isdst ? '15' : '16',
-        minute    => 0,
+        hour      => $cut_time[0],
+        minute    => $cut_time[1],
         second    => 0,
     );
 
@@ -55,3 +73,21 @@ __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
+=head1 NAME
+
+OpenXPKI::Connector::FakeValidity;
+
+=head1 DESCRIPTION
+
+Connector to align a validity time spec to a fixed point in the week.
+
+=head2 Configuration
+
+=item cut_wday
+
+The day of week as used by perl localtime, default is 6 (Saturday).
+Note that Sunday is 0!
+
+=item cut_time
+
+The time of the day, given as hh:mm, default is 17:00.
